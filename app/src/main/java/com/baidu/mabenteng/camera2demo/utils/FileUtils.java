6cbe66b2a26ebe4167a79c9f8263e776971131fe
mabenteng <mabenteng@baidu.com>
2018-09-19 15:56:42 +0800

    CameraAPI可以拍照了现在

---
+++ app/src/main/java/com/baidu/mabenteng/camera2demo/utils/FileUtils.java
@@ -0,0 +1,10 @@
+package com.baidu.mabenteng.camera2demo.utils;
+
+/**
+ * @author mabenteng
+ * @version v10.10.0
+ * Created by AndroidStudio
+ * @since 2018/9/17
+ */
+public class FileUtils {
+}
